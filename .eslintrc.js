72a2385021ccc95c93cfea74152fae0aa7e0ff4c
Chad Hietala <chietala@linkedin.com>
2021-10-25 15:01:59 -0400

    fix linting for windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   overrides: [
     {
